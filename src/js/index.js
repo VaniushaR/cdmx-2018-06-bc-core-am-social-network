6906b138ea44b6e2284b4b5b81e6b7bda6b2ee59
VaniushaR <vaniusha.ra@gmail.com>
2018-07-23 19:04:02 -0500

    Profile view

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
-const drawlogin =()=> {
-const login = document.getElementById("login");
-// Draw inputs in Index
+const drawlogin = () => {
+  const login = document.getElementById("login");
+  // Draw inputs in Index
   login.innerHTML = `<h3 id="titulos2">Sign in</h3>
   <input class="validate" type="text" placeholder="Nombre..." id ="nick">
   <input class="validate" type="text" placeholder="Email..." id ="email">
@@ -8,14 +8,12 @@ const login = document.getElementById("login");
   <input class="waves-effect waves-light btn" class="btn-floating pulse"  type="button" value="Iniciar Sesión" id="sesion">`;
 };
 
-
-const drawlogout =()=> {
-const logout = document.getElementById("userlogout");
-let mail = document.getElementById("email").value;
-// Draw button logout
-logout.innerHTML = `<label class="waves-effect waves-light btn" class="btn-floating pulse" id="user_id"></label><input type="button" value="Cerrar sesion" id="logout">`;
+const drawlogout = () => {
+  const logout = document.getElementById("userlogout");
+  let mail = document.getElementById("email").value;
+  // Draw button logout
+  logout.innerHTML = `<label class="waves-effect waves-light btn" class="btn-floating pulse" id="user_id"></label><input type="button" value="Cerrar sesion" id="logout">`;
 };
-
 
 window.onload = drawlogin();
 window.onload = drawlogout();
@@ -23,43 +21,45 @@ window.onload = drawlogout();
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     // User is signed in.
-    document.getElementById("userlogout").style.display ="display";
-    document.getElementById("login").style.display ="none";
+    document.getElementById("userlogout").style.display = "display";
+    document.getElementById("login").style.display = "none";
     let user = firebase.auth().currentUser;
     if (user != null) {
       let email_id = user.email;
-      document.getElementById("user_id").innerHTML = `Welcome User: ${email_id}`;
-
+      document.getElementById(
+        "user_id"
+      ).innerHTML = `Welcome User: ${email_id}`;
     }
   } else {
     // No user is signed in.
 
-    document.getElementById("userlogout").style.display ="none";
-    document.getElementById("login").style.display ="display";
+    document.getElementById("userlogout").style.display = "none";
+    document.getElementById("login").style.display = "display";
   }
 });
 
-const getdata =()=> {
-const mail = document.getElementById("email").value;
-console.log(mail);
-const password = document.getElementById("password").value;
+const getdata = () => {
+  const mail = document.getElementById("email").value;
+  console.log(mail);
+  const password = document.getElementById("password").value;
 
-firebase.auth().signInWithEmailAndPassword(mail, password).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  alert("Error "+ errorMessage);
-  // ...
-});
-
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(mail, password)
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      alert("Error " + errorMessage);
+      // ...
+    });
 };
 
-const logout =()=> {
-  firebase.auth().signOut()
+const logout = () => {
+  firebase.auth().signOut();
 };
-
 
 let sesion = document.getElementById("sesion");
 let unsesion = document.getElementById("logout");
-sesion.addEventListener("click",getdata);
+sesion.addEventListener("click", getdata);
 unsesion.addEventListener("click", logout);
