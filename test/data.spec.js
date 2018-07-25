b02bdbdae57bbc9f975bca282fefb42f279b3d14
VaniushaR <vaniusha.ra@gmail.com>
2018-07-24 19:40:58 -0500

    Testing first update

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,1 +1,9 @@
-// Ejecuta tu test 
+// Ejecuta tu test
+describe('network', () => {
+  it('debe contener una función loginGoogle, para logear al usuario mediante Google,  en el objeto global network', () => {
+    assert.isFunction(loginGoogle);
+  });
+  it('debe contener una función loginFacebook, para logear al usuario mediante Facebook, en el objeto global network', () => {
+    assert.isFunction(loginFacebook);
+  });
+});
