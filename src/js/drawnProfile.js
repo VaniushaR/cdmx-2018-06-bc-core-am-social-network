de5c04336901c63660541d4493d18f6478fb86c9
VaniushaR <vaniusha.ra@gmail.com>
2018-07-26 10:36:18 -0500

    Animación de menú publicar desplegable, y detalles de responsive

--- src/js/drawnProfile.js
+++ src/js/drawnProfile.js
@@ -1,9 +1,23 @@
-
 //side navbar iniatization with JQuery
 $(document).ready(function() {
-  $(".sidenav").sidenav();
+  $('.sidenav').sidenav();
 });
-//end of the navbar activation
+
+//activation of side down panel display
+$(document).ready(function() {
+  $('#flip').click(function() {
+    $('#panel').slideToggle('slow');
+  });
+});
+
+/* <section id="panel">
+                        <a>
+                            <i class="material-icons icon-pad">add_location</i>
+                        </a>
+                        <a>
+                            <i class="material-icons icon-pad">add_a_photo</i>
+                        </a>
+                    </section> */
 
 //login con google y firebase
 /*const provider = new firebase.auth.GoogleAuthProvider();
