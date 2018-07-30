0e6a0331af23be03de7b5c1b0c9010ffdc16c8f3
Your Name <you@example.com>
2018-07-30 16:37:30 -0500

    Add CRUD

--- src/js/index.js
+++ src/js/index.js
@@ -1,65 +1,50 @@
-const drawlogin =()=> {
-const login = document.getElementById("login");
-// Draw inputs in Index
-  login.innerHTML = `<input type='text' placeholder='Nickname...' id ='nick'>
-  <input type='text' placeholder='Email...' id ='email'>
-  <input type='password' placeholder='Password...' id = 'password'>
-  <input type='button' value='Iniciar Sesión' id='sesion'>`;
+const drawlogin = () => {
+  let login = document.getElementById('login');
+  // Draw inputs in Index
+  login.innerHTML = `
+<h5 id='titulos2'>
+Sign in
+</h5>
+<input class='validate col s10 m8 l6 offset-s1 offset-m2 offset-l3' type='text' placeholder='Email...' id='email'>
+<input class='validate col s10 m8 l6 offset-s1 offset-m2 offset-l3' type='password' placeholder='Password...' id='password'>
+<div class="row">
+<a id='sesion' class="waves-effect waves-light btn-large col s10 m8 l6 offset-s1 offset-m2 offset-l3">Iniciar Sesión</a>
+</div>
+<div class="row">
+<a id='sesionGoogle' class="waves-effect red darken-4 btn-large col s10 m8 l6 offset-s1 offset-m2 offset-l3">Google</a>
+</div>
+<div class="row">
+<a id='sesionFacebook' class="waves-effect indigo darken-3 btn-large col s10 m8 l6 offset-s1 offset-m2 offset-l3">
+    facebook
+</a>
+</div>
+`;
 };
-
-
-const drawlogout = ()=> {
-const logout = document.getElementById('userlogout');
-let mail = document.getElementById('email').value;
-=======
-  login.innerHTML = `<h3 id="titulos2">Sign in</h3>
-  <input class="validate" type="text" placeholder="Nombre..." id ="nick">
-  <input class="validate" type="text" placeholder="Email..." id ="email">
-  <input class="validate" type="password" placeholder="Password..." id = "password">
-  <input class="waves-effect waves-light btn" class="btn-floating pulse"  type="button" value="Iniciar Sesión" id="sesion">`;
-};
-
-
 
 
 drawlogin();
-drawlogout();
 
-firebase.auth().onAuthStateChanged(function(user) {
+// Entrar a la Database
+firebase.auth().onAuthStateChanged(user => {
   if (user) {
     // User is signed in.
-    document.getElementById("userlogout").style.display ="display";
-    document.getElementById("login").style.display ="none";
     let user = firebase.auth().currentUser;
     if (user != null) {
       let email_id = user.email;
-<<<<<<< HEAD
-      document.getElementById('user_id').innerHTML = `Welcome Mexican Lover: ${email_id}`;
-=======
-      document.getElementById("user_id").innerHTML = `Welcome User: ${email_id}`;
->>>>>>> 9c554cf6157bd5eaf22454d3ab30aa98a497ef38
-
+      location.href = ('views/view1.html');
     }
   } else {
-    // No user is signed in.
 
-    document.getElementById("userlogout").style.display ="none";
-    document.getElementById("login").style.display ="display";
+
   }
 });
 
 const getdata =()=> {
-<<<<<<< HEAD
 const mail = document.getElementById('email');
 const password = document.getElementById('password');
 const nick = document.getElementById('nick');
-=======
-const mail = document.getElementById("email").value;
-console.log(mail);
-const password = document.getElementById("password").value;
->>>>>>> 9c554cf6157bd5eaf22454d3ab30aa98a497ef38
 
-firebase.auth().signInWithEmailAndPassword(mail.value, password.value).catch(function(error) {
+firebase.auth().signInWithEmailAndPassword(mail.value, password.value).catch(error=> {
   // Handle Errors here.
   var errorCode = error.code;
   var errorMessage = error.message;
@@ -67,14 +52,26 @@ firebase.auth().signInWithEmailAndPassword(mail.value, password.value).catch(fun
   // ...
 });
 
-};
-
-const logout =()=> {
-  firebase.auth().signOut()
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(mail, password)
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      alert("Error " + errorMessage);
+      // ...
+    });
 };
 
 
 let sesion = document.getElementById("sesion");
-let unsesion = document.getElementById("logout");
 sesion.addEventListener("click",getdata);
-unsesion.addEventListener("click", logout);
+let google = document.getElementById('sesionGoogle');
+google.addEventListener('click', event => {
+network.loginGoogle();
+    });
+let facebook = document.getElementById('sesionFacebook');
+  facebook.addEventListener('click', event => {
+  network.loginFacebook();
+      });
